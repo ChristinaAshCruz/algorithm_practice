705d3931801f6ffa800b04eff18aeff5e74a5470
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-04-09 00:32:23 -0700

    more algo practice with java

---
+++ java_pratice/Practice.java
@@ -0,0 +1,22 @@
+public class Practice {
+	public static void main(String[] args) {
+		System.out.println("Setting and swapping");
+        // Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+        String myName = "Christina";
+        System.out.println(myName);
+        // This it is not possible to turn "Christina" into an int data type
+        System.out.println("------------------------------");
+        
+        System.out.println("Print -52 to 1066");
+        // Print integers from -52 to 1066 using a FOR loop.
+        for(int i=-52; i<=1066; i++){
+            System.out.println(i);
+        }
+        
+        System.out.println("------------------------------");
+
+        System.out.println("Don’t Worry, Be Happy");
+        // Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+        public 
+	}
+}
