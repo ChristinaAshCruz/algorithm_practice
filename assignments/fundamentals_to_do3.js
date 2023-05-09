6597513a535ed3ce2b46b2e9d6dfb407b34b998b
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-05-08 23:06:08 -0700

    more daily algo practice

--- assignments/fundamentals_to_do3.js
+++ assignments/fundamentals_to_do3.js
@@ -2,10 +2,10 @@
 // Given an array, write a function that changes all positive numbers in the array to “big”.
 // Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1,"big","big",-5].
 function biggieSize(arr) {
-  // first, we need to create an empty array variable to hold our new array values
-  var newArray = [];
+  // first, we need to create an empty array letiable to hold our new array values
+  let newArray = [];
   // we need to create a loop that will go through the array's values
-  var i = 0;
+  let i = 0;
   while (i < arr.length) {
     // during the loop, we have to use an if/else statement that will check whether the values are positive or not
     if (arr[i] >= 0) {
@@ -18,17 +18,17 @@ function biggieSize(arr) {
   return newArray;
 }
 
-var arr1 = [-1, 3, 5, -5];
+let arr1 = [-1, 3, 5, -5];
 console.log(biggieSize(arr1));
 
 // PRINT LOW, RETURN HIGH
 // Create a function that takes an array of numbers. The function should print the lowest value in the array, and return the highest value in the array.
 
 function printLowReturnHigh(arr) {
-  var lowVal = arr[0];
-  var highVal = arr[0];
+  let lowVal = arr[0];
+  let highVal = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] < lowVal) {
       lowVal = arr[i];
     }
@@ -40,8 +40,8 @@ function printLowReturnHigh(arr) {
   return "Highest value: " + highVal;
 }
 
-var arr1 = [-1, 3, 5, -5];
-var arr2 = [10, 55, 32, 5, 7, 1, -20];
+let arr1 = [-1, 3, 5, -5];
+let arr2 = [10, 55, 32, 5, 7, 1, -20];
 console.log(printLowReturnHigh(arr1));
 console.log(printLowReturnHigh(arr2));
 
@@ -49,14 +49,33 @@ console.log(printLowReturnHigh(arr2));
 // Build a function that takes an array of numbers. The function should print the second-to-last value in the array, and return first odd value in the array.
 
 function printOneReturnAnother(arr) {
-  var secondToLastVal = arr[arr.length - 2];
+  let secondToLastVal = arr[arr.length - 2];
   console.log("2nd To Last Value: " + secondToLastVal);
 
-  var firstOddVal = arr.find((num) => num % 2 != 0);
+  let firstOddVal = arr.find((num) => num % 2 != 0);
   return "First Odd Value: " + firstOddVal;
 }
 
-var arr2 = [10, 55, 32, 5, 7, 1, -20];
+let arr2 = [10, 55, 32, 5, 7, 1, -20];
 console.log(printOneReturnAnother(arr2));
 
 // DOUBLE VISION
+// Given an array, create a function to return a new array where each value in the original has been doubled. Calling double([1,2,3]) should return [2,4,6] without changing original.
+
+let arr1 = [-1, 3, 5, -5];
+function doubleVision(arr) {
+  // we need to create an empty array to hold our new values
+  // we'll also return this new array at the end of the function
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let newVal = arr[i] * 2;
+    newArray.push(newVal);
+  }
+  return newArray;
+}
+
+let array = arr1;
+let arrayDoubled = doubleVision(arr1);
+
+console.log(array);
+console.log(arrayDoubled);
